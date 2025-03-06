194ca2368d1a2db065541c24a632332ee0f113b1
Jacques Steenkamp <jsteenkamp@mauriceblackburn.com.au>
2025-03-06 15:23:46 +1100

    vscode setup

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,46 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+/**
+ * Create a team object.
+ */
+
+const team = {
+  name: "WeDo With ShareDo",
+  teamSize: 10,
+  color: "grey",
+  pocketNum: 15,
+  subTeamSize: {
+    devSize: 5,
+    testSize: 5,
+  },
+  TeamIsAtWork: true,
+  toggleWorking: function (workStatus) {
+    this.TeamIsAtWork = workStatus;
+  },
+  newSubTeam: function (dev, test) {
+    this.subTeamSize.devSize = dev;
+    this.subTeamSize.testSize = test;
+  },
+};
+
+const markup = (team) => {
+  return `
+    <div>
+      <h3>${team.name}</h3>
+      <ul>
+        <li>Size: ${team.teamSize}</li>
+        <li>Color: ${team.color}</li>
+        <li>Number of pockets: ${team.pocketNum}</li>
+        <li>Teams: Dev: ${team.subTeamSize.devSize}, Test: ${
+    team.subTeamSize.testSize
+  } </li>
+        <li>Team at work: ${team.toggleWorking ? "Yes" : "No"}</li>
+      </ul>
+    </div>
+  `;
+};
+
+const main = document.createElement("main");
+main.innerHTML = markup(team);
+document.body.appendChild(main);
--- install-extensions.bash
+++ install-extensions.bash
@@ -1,4 +1,14 @@
 code --install-extension dbaeumer.vscode-eslint --force &
 code --install-extension esbenp.prettier-vscode --force &
+code --install-extension github.copilot --force &
+code --install-extension github.copilot-chat --force &
+code --install-extension idered.npm --force &
+code --install-extension mauriciosoares.localtunnel-for-vscode --force &
+code --install-extension ms-azuretools.vscode-docker --force &
+code --install-extension ms-edgedevtools.vscode-edge-devtools --force &
+code --install-extension ms-vscode-remote.remote-containers --force &
 code --install-extension ms-vscode-remote.remote-wsl --force &
-code --install-extension ritwickdey.liveserver --force
+code --install-extension ms-vsliveshare.vsliveshare --force &
+code --install-extension postman.postman-for-vscode --force &
+code --install-extension ritwickdey.liveserver --force &
+code --install-extension sharedo.sharedo-vscode-extension --force
